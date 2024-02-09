0cf6350f6f88ff32ad9f298977937be4eecc79da
pfaversa <pablo.aversa@wizeline.com>
2024-02-09 14:59:05 -0600

    verify invalid login scenarios

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -15,4 +15,30 @@ data.login_credential.forEach((credentials: { username: string, password: string
     await loginPage.login(credentials.username, credentials.password);
     await expect(homePage.container).toBeVisible();
   });
+
+  test('Test Case: Invalid login', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.login(credentials.username, 'Test@!"#!"#123');
+    await expect(page.getByText('Wrong email or password.')).toBeVisible();
+  });
+
+  test('Test Case: Unsuccessful login / E-mail Missing', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.login('', 'Test@!"#!"#123');
+    await expect(page.getByText('Please enter a valid email address.')).toBeVisible();
+  });
+
+  test('Test Case: Unsuccessful login / Password Missing', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.login(credentials.username, '');
+    await expect(page.getByText('Passwords must be at least 8 characters long.')).toBeVisible();
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+  
 });
