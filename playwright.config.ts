66408193f6cc5133a0cfe02c50acd8c05e24bdb4
MoisesRivasNavarroWizeline <moises.rivas@wizeline.com>
2024-02-08 18:55:20 -0600

    pom change

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,8 +28,6 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    browserName: "chromium",
-    headless: false,
   },
 
   /* Configure projects for major browsers */
